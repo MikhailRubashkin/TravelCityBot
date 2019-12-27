d253d118ac367604ac2f916fd84f06c9a3e0cbcd
rubashkin_mikhail@tut.by <pristijpribor@mail.ru>
2019-12-27 15:34:26 +0300

    TelegramCityService

--- src/main/java/com/example/travelcitybot/repos/CityRepo.java
+++ src/main/java/com/example/travelcitybot/repos/CityRepo.java
@@ -1,9 +1,11 @@
 package com.example.travelcitybot.repos;
 
 import com.example.travelcitybot.domain.TelegramCity;
-        import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
 
 public interface CityRepo extends CrudRepository<TelegramCity, Long> {
 
-    //List<TelegramCity> findByCity( String city);
+    List<TelegramCity> findByTelegramCity( String telegramCity);
 }
--- src/main/java/com/example/travelcitybot/service/TelegramCityService.java
+++ src/main/java/com/example/travelcitybot/service/TelegramCityService.java
@@ -1,4 +1,38 @@
 package com.example.travelcitybot.service;
 
+import com.example.travelcitybot.domain.TelegramCity;
+import com.example.travelcitybot.repos.CityRepo;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.List;
+
+@Service
 public class TelegramCityService {
+
+    private final CityRepo cityRepo;
+
+    public TelegramCityService ( CityRepo cityRepo ){
+        this.cityRepo = cityRepo;
+    }
+
+    @Transactional
+    public List<TelegramCity> findByCity ( String telegramCity ){
+        return cityRepo.findByTelegramCity (telegramCity);
+    }
+
+    @Transactional
+    public void addTelegramCity ( TelegramCity telegramCity ){
+        cityRepo.save (telegramCity);
+    }
+
+    @Transactional
+    public void updateTelegramCity ( TelegramCity telegramCity ){
+        cityRepo.save (telegramCity);
+    }
+
+    @Transactional
+    public void deleteTelegramCity ( Long id ){
+        cityRepo.deleteById (id);
+    }
 }
